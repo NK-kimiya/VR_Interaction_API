79d276737b00264ee1f1d702e1a85711971cc09a
kimi <kinarishige26@gmail.com>
2023-12-29 15:44:27 +0900

    v1.0_build.shでRenderの仮想環境のパスを変更

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
-/opt/render/project/src/.venv/bin/python3.8 -m pip install --upgrade pip
+/opt/render/project/src/.venv/bin/python3.11 -m pip install --upgrade pip
 set -o errexit
 
 pip install -r requirements.txt
